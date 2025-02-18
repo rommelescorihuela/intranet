20f90fe093046609820f99d3f63c1d957107f8cf
rommel escorihuela <rommelescorihuela@gmail.com>
2025-02-18 15:06:57 -0400

    inicializando filament

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,6 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\Filament\AdministrationPanelProvider::class,
     App\Providers\FortifyServiceProvider::class,
     App\Providers\JetstreamServiceProvider::class,
 ];
